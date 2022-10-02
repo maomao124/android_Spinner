640a3c6c9bd9fddec44797943fcf6079b196f6af
mao <1296193245@qq.com>
2022-10-02 21:57:02 +0800

    android_Spinner

--- app/src/main/java/mao/android_spinner/MainActivity2.java
+++ app/src/main/java/mao/android_spinner/MainActivity2.java
@@ -3,8 +3,11 @@ package mao.android_spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity
 {
@@ -17,7 +20,44 @@ public class MainActivity2 extends AppCompatActivity
 
         Spinner spinner = findViewById(R.id.Spinner1);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, new String[]{"1", "2"});
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, new String[]{"1", "2"});
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.item_select,
+                new String[]
+                        {
+                                "选项1",
+                                "选项2",
+                                "选项3",
+                                "选项4",
+                                "选项5",
+                                "选项6",
+                                "选项7"
+                        }
+        );
         spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                toastShow("当前选择的是第" + (position + 1) + "个");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+                toastShow("取消选择");
+            }
+        });
+    }
+
+    /**
+     * 显示消息
+     *
+     * @param message 消息
+     */
+    private void toastShow(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
